6996bc5eec1aeac492ad6edb63ac49d52a61e6d6
ecourse <smkang@codenuri.co.kr>
2023-01-04 14:36:58 +0900

    Add files via upload

---
+++ DAY1/10_method2.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+// Point 는 클래스이므로 reference 타입입니다.
+class Point
+{
+    public int x;
+    public int y;
+    public Point(int a, int b) { x = a; y = b; }
+}
+class Program
+{
+    // call by value 입니다.
+    public static void F1(Point p1)     { p1.x = 10; }
+    public static void F2(ref Point p1) { p1.x = 10; }
+
+    public static void Main()
+    {
+        // Point p = new Point(1, 2);
+
+        // F1(p);  // p의 값(객체의 주소)을 p1 으로 복사하게 됩니다.
+        // 주소값만 복사되므로, 결국 같은 객체를 가리키게 됩니다.
+        // F2(ref p); // 위 코드와 동일한 효과 입니다.
+
+        // 핵심 1. reference type 의 객체를 call by value로 전달하면
+        // => 원본 객체의 상태 변경은 가능합니다.
+        // => 하지만 새로운 객체를 담아 올수는 없습니다.
+        // F3(p);  
+
+        // 핵심 2. 참조 변수에 새로운 객체를 담아 오려면 
+        //         참조 변수를 다시 참조로 전달해야 합니다.
+
+        Point p = new Point(1, 2);
+
+        F4(ref p); // 객체의 주소가 아닌 참조 변수 p의 메모리 주소를 전달.
+
+        Console.WriteLine(p.x); // ??
+    }
+
+    public static void F4(ref Point p1)
+    {
+        // 현재 p1 은 Main 에서 만든 객체의 참조(주소)가 아니라!!
+        // 참조 변수의 참조(p 의 주소) 입니다.
+        // 아래 코드는 
+        // C++언어 : *p1 = new Point(10,20);
+        //           p1이 가리키는 메모리 = new Point(10,20)
+        p1 = new Point(10, 20);
+            // => 새로 만든 객체의 주소를 p1에 넣는 코드가 아닙니다.
+            // => p1이 가리키는 곳(main 함수의 p참조변수)에 새로운 객체의
+            //    주소를 넣게 됩니다.
+         
+    }
+
+    public static void F3(Point p1)
+    {
+        p1 = new Point(10, 20);
+    }
+}
---
+++ DAY1/10_method3.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+// 3. param
+
+class Program
+{
+    // 이 함수는 "정수 2개"를 전달해야 합니다.
+    public static void f1(int a, int b )
+    {
+    }
+
+    // 핵심 1. 동일 타입의 객체를 여러개(가변) 전달 받으려면
+    // => 배열을 사용하면 됩니다.
+    public static void f2(int[] arr)
+    {
+        foreach (var n in arr)
+            Console.WriteLine(n);
+    }
+
+    // 핵심 2. param 를 사용하면 호출자가 배열이 아닌
+    //        "10,20,30" 형태로 전달 가능합니다.
+    public static void f3(params int[] arr)
+    {
+        foreach (var n in arr)
+            Console.WriteLine(n);
+    }
+
+    public static void Main()
+    {
+        f2(new int[] { 10, 20, 30 });
+        //f2(10, 20, 30);
+
+        f3(new int[] { 10, 20, 30 });
+        f3(10, 20, 30); // 인자가 params 라면 
+                        // "10, 20, 30" => "new int[]{10,20,30}" 으로 컴파일러가 변경
+
+//      f5(10, 20, 30);
+    }
+
+    // 주의 사항 - params 는 마지막 인자로만 사용가능.
+//  public static void f4(int a, params int[] arr) { } // ok
+//  public static void f5(params int[] arr, int n) { } // error
+}
