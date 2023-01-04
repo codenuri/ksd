6cd520914106ab51f8497833b62850fd2aaf4ad9
ecourse <smkang@codenuri.co.kr>
2023-01-04 15:01:13 +0900

    Add files via upload

---
+++ DAY1/10_method4.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+// C# 4.0 부터 디폴트 파라미터 가능. 선택적 파라미터(Optional Parameter)라고 부름.
+class Program
+{
+    // C++, C#, Python : 지원
+    // java, swift : 지원 안함
+    public static void goo(int x = 0, int y = 0, int z = 0) { }
+
+    public static void Main()
+    {
+        goo(1, 2, 3);
+        goo(1, 2);      // goo(1, 2, 0) 의 의미
+        goo(1);
+
+        // 아래 함수 호출을 보고 인자의 의미를 생각해 봅시다.
+        // SetRect(10, 10, 30, 30); // x, y, w, h 일수도 있지만
+        //                              x1, y1, x2, y2 일수도 있습니다.
+
+        // 함수 호출을 아래처럼 할수 있으면 어떨까요 ?
+        // SetRect(10, 10, width: 30, height: 30);
+                // objective-c 언어의 개념
+                // swift 와 C# 도 지원 됩니다.
+
+
+        // C# 에서도 파라미터 전달시 이름 사용가능 합니다.
+        // 이름이 있으면 순서 변경 가능합니다.
+        goo(x: 1, y: 2, z: 3);
+        goo(1, z: 3, y: 2);
+        goo(y: 2);
+
+
+        //
+        Base b = new Derived();
+
+        b.foo();
+
+    }
+
+}
+
+
+class Base
+{
+    public virtual void foo(int n = 10) { Console.WriteLine($"Base : {n}"); }
+}
+class Derived : Base
+{
+    public override void foo(int n = 20) { Console.WriteLine($"Derived : {n}"); }
+}
---
+++ DAY1/10_method5.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+// 식본문 메소드(expression-bodied method)
+
+class Program
+{
+    public static int square1(int x) { return x * x; }
+
+    // 함수 구현이 간단하고 반환 값 정도만 있다면
+    // 아래 처럼 한줄로 만들어도 됩니다.
+    // => expression-bodied method 라고 합니다.
+    // => Rust 등의 최신 언어에서도 널리 사용되므로 꼭 알아 두세요.
+    // => 권장 : statement(문장) vs expression(구문, 표현식) 의 차이점을 학습해 보세요
+    public static int square2(int x) => x * x;
+
+    // Rust 표기법 : return 과 ;가 없습니다.
+    // public static int square2(int x) { x * x }; 
+
+    public static void Main()
+    {
+        Console.WriteLine(square1(3));
+    }
+
+}
