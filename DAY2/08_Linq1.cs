755f6a0014cbfd14a272e990f4548139ba82b7b7
ecourse <smkang@codenuri.co.kr>
2023-01-05 15:57:02 +0900

    Add files via upload

---
+++ DAY2/08_Linq1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        // 홀수를 출력하는 방법
+        // 1. foreach 또는 반복자 사용
+        foreach( var n in arr )
+        {
+            if ( n % 2 == 1)
+                Console.WriteLine(n);
+        }
+
+        // 2. Linq 를 사용. ( Language INtegrated Query )
+        // Where : arr의 모든 요소를 단항 조건자 함수에 보내서 참을 반환하는 것만 선택
+        //var odds = arr.Where(조건자함수);
+
+        // var odds = arr.Where(Foo);  // 함수를 직접 전달하는 경우
+        
+        var odds = arr.Where( n => n % 2 == 1);    // 람다 표현식을 사용하는 경우
+
+        foreach (var n in odds)
+        {       
+            Console.WriteLine(n);
+        }
+    }
+
+    public static bool Foo(int n) {  return n % 2 == 1; }
+}
---
+++ DAY2/08_Linq2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        string[] arr = { "kimmm", "park", "choi", "lee", "jung" };
+
+        // Fluent Syntax : Where, OrderBy, Select
+        /*
+        var names = arr.Where(s => s.Contains('i'))
+                       .OrderBy(s => s.Length)
+                       .Select(s => s.ToUpper());
+        */
+
+        // 아래 코드는 위와 동일합니다. 컴파일러가 위 코드로 변경합니다.
+        // => 마치 SQL 문 처럼 보입니다.
+        var names = from s in arr where (s.Contains('i')) select s.ToUpper();
+
+
+        foreach (var n in names)
+        {
+            Console.WriteLine(n);
+        }
+
+    }
+}
