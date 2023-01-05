0d9442b948140a36df03a0e6cb30b0bd876cba62
ecourse <smkang@codenuri.co.kr>
2023-01-05 17:18:22 +0900

    Add files via upload

---
+++ DAY2/step0.cs
@@ -0,0 +1,10 @@
+﻿// step0 : C# 기본 코드
+using System;
+
+class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Hello, WPF");
+    }
+}
---
+++ DAY2/step1.cs
@@ -0,0 +1,25 @@
+﻿// step1
+
+// 핵심 1. WPF 를 사용하기 위한 어셈블리
+// PresentationCore
+// PresentationFramework
+// System
+// System.Xaml
+// WindowsBase
+
+// 핵심 2. WPF 프로그램이 지켜야 하는 규칙
+// => System.Windows.Application 에서 파생 된 클래스 만들고
+// => 객체를 생성후, Run() 호출
+// => 관례적으로 Main 메소드도 클래스에 추가 합니다.
+
+using System;
+using System.Windows;
+
+class App : Application
+{
+    public static void Main()
+    {
+        App app = new App();
+        app.Run();
+    }
+}
