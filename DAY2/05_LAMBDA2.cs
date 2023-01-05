7cd6afe27644e510d87aa54b8c1e7cd2a2ef91b0
ecourse <smkang@codenuri.co.kr>
2023-01-05 14:13:43 +0900

    Add files via upload

---
+++ DAY2/05_LAMBDA2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Button
+{
+    public event Action ClickAction = null;
+
+    public void Click()
+    {
+        if (ClickAction != null)
+            ClickAction();
+    }
+}
+
+class Program
+{
+    public static void Foo() { Console.WriteLine("Foo"); }
+    public static void Goo() { Console.WriteLine("Goo"); }
+
+    public static void Main()
+    {
+        Button b = new Button();
+
+        b.ClickAction += Foo;
+
+        // 람다 표현식으로 버튼의 이벤트 핸들러 등록
+        // => 버튼 누를때 하는일이 간단한경우, 이렇게 처리하는 경우도 많음
+        b.ClickAction += () => Console.WriteLine("버튼 눌림");
+
+        b.Click();
+    }
+}
---
+++ DAY2/05_lambda.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    public static int Add(int a, int b) {  return a + b; }
+
+    public static void Main()
+    {
+        int v1 = 10;
+
+        // 1. 함수를 만들어서 delegate 에 등록.
+        Func<int, int, int> plus = Add;
+
+        // 2. 람다표현식을 사용해서 익명의 함수를 delegate 에 등록
+        // 람다표현식 표기법 : (인자리스트) => { 함수 구현 }
+
+        // Func<int, int, int> plus2 = (int a, int b) => {  return a + b; };
+        // Func<int, int, int> plus2 = (a, b) => { return a + b; };
+        Func<int, int, int> plus2 = (a, b) => a + b; // C#
+//      Func<int, int, int> plus2 = $0 + $1;         // Swift
+
+        Console.WriteLine($"{plus2(10,20)}");
+    }
+}
