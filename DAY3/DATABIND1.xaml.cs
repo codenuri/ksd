2ac05d0a6974c9081bcd02ce2bccf0c24493e493
ecourse <smkang@codenuri.co.kr>
2023-01-06 14:52:48 +0900

    Add files via upload

---
+++ DAY3/DATABIND1.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF06_BIND2
+{
+    /// <summary>
+    /// DATABIND1.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class DATABIND1 : Window
+    {
+        public DATABIND1()
+        {
+            InitializeComponent();
+
+            Person p = new Person();
+
+            p.Name = "kim";
+            p.Address = "pusan";
+
+            stackpanel.DataContext = p; // 데이타 공급원은 "객체 p" 이다.
+        }
+    }
+}
---
+++ DAY3/Person1.cs
@@ -0,0 +1,19 @@
+﻿// Person1.cs
+using System;
+
+public class Person
+{
+    public string name;
+    public string addr;
+
+    public String Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+    public String Address
+    {
+        get { return addr; }
+        set { addr = value; }
+    }
+}
