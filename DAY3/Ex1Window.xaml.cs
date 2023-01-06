46b75591c00929627ddebac4ff6301d600b3ba07
ecourse <smkang@codenuri.co.kr>
2023-01-06 11:01:34 +0900

    Add files via upload

---
+++ DAY3/Ex1Window.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF02
+{
+    /// <summary>
+    /// Ex1Window.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Ex1Window : Window
+    {
+        public Ex1Window()
+        {
+            // InitializeComponent 가 XAML을 load 합니다.
+            // GUI 가 완전히 완성된후해야 하는 작업은
+            // 반드시 InitializeComponent 아래 부분에서 하세요.
+
+            InitializeComponent();
+
+            // XAML 로 하는 모든 작업은 항상 C# 코드로도 할수 있습니다.
+            this.Title = "WPF02";
+        }
+    }
+}
---
+++ DAY3/Ex3Window.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF02
+{
+    // MyColor 의 객체를 생성해서 "약속된 함수인 ProvideValue" 를 호출하면
+    // "노란색 브러시" 를 얻게 됩니다.
+    public class MyColor : MarkupExtension
+    {
+        private string key = null;
+
+        public MyColor(string s) { key = s; }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            switch(key)
+            {
+                case "TitleColor": return new SolidColorBrush(Colors.Red);
+                case "HeaderColor": return new SolidColorBrush(Colors.Blue);
+            }
+            return new SolidColorBrush(Colors.Yellow);
+        }
+    }
+
+
+
+
+
+    /// <summary>
+    /// Ex3Window.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Ex3Window : Window
+    {
+        public Ex3Window()
+        {
+            InitializeComponent();
+        }
+    }
+}
