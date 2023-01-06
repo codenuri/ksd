adea900c9c57ae71d0c22d649f545a37dd59c758
ecourse <smkang@codenuri.co.kr>
2023-01-06 13:56:21 +0900

    Add files via upload

---
+++ DAY3/RESOURCE1.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF04_RESOURCE
+{
+    // 프로그램에서 사용할 브러시 및 다양한 객체를 static 멤버로 제공하는 클래스
+    public class MyConstant
+    {
+        public static Brush brush1 = new SolidColorBrush(Colors.Yellow);
+        public static Brush brush2 = new SolidColorBrush(Colors.Red);
+
+        public static double fontsize1 = 18;
+    }
+
+
+
+    public partial class RESOURCE1 : Window
+    {
+        public RESOURCE1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DAY3/RESOURCE2.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF04_RESOURCE
+{
+    /// <summary>
+    /// RESOURCE2.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class RESOURCE2 : Window
+    {
+        public RESOURCE2()
+        {
+            InitializeComponent();
+
+            // this : Main Window 입니다.
+
+            // Window 는 다양한 속성이 있습니다.
+            this.Title = "AAA";
+
+            // 핵심 : 윈도우 속성중에 Resource 라는 "Dictionary" 가 있습니다.
+            // => 자주 사용되는 자원을 미리 생성해 놓고 사용할수 있습니다.
+            // this.Resources["brush1"] = new SolidColorBrush(Colors.Red);
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Resources["brush2"] = new SolidColorBrush(Colors.Blue);
+        }
+    }
+}
